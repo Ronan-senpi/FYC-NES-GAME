cf02d5cbbba2e4b18e1ae5d75d3f1f9f7382846e
Ronan <r.dhersignerie@gmail.com>
2022-01-09 23:17:11 +0100

    palette in files

--- hello-background.asm
+++ hello-background.asm
@@ -170,10 +170,7 @@ a_gauche         ; Si on va à gauche,
   JMP mainloop
 
 
-palette:
-  DC.B $21,$00,$10,$30,$21,$01,$21,$31,$21,$36,$16,$0f,$21,$09,$19,$29
-  DC.B $21,$00,$10,$30,$21,$01,$21,$31,$21,$36,$16,$0f,$21,$09,$19,$29
-
+.include "palette.asm"
 .include "nametables.asm"
 
 ; vecteurs d interruptions du 6502, la nes possède 3 vecteurs d'interruptions, on ne se servira que de RESET et de VBL ici
